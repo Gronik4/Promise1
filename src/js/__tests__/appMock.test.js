d96dc511d9a187fb0fb57b87b5f3d7fddaa76535
Gronik64 <golegni@yandex.ru>
2021-12-14 20:23:00 +0300

    Правки по замечаниям

--- src/js/__tests__/appMock.test.js
+++ src/js/__tests__/appMock.test.js
@@ -1,9 +1,9 @@
 import savingGame from '../app';
 
 jest.mock('../reader.js');
-const Expected = 'Ошибка';
 
 test('Test app on throw', async () => {
+  const Expected = 'Ошибка';
   const date = await savingGame();
   expect(date).toBe(Expected);
 });
